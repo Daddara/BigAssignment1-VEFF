32f805e207dce18099d35d8bd4aec7cf7d01a02a
hordurh20 <hordurh20@ru.is>
2022-02-01 12:57:14 +0000

    8 9 10 complete

--- MakeBelieveJS.js
+++ MakeBelieveJS.js
@@ -52,6 +52,45 @@
     }
 }
 
+    MakeBelieveJS.prototype.insertText = function(text){
+        // console.log("Inside text: ", text);
+        // console.log(this.elems);
+        for(var i = 0; i < this.elems.length; i++){
+            this.elems[i].textContent = text;
+        }
+        return this;
+        // this.elems[0].textContent = text;
+        
+    }
+
+    MakeBelieveJS.prototype.append = function(string){
+        if(typeof string == "string"){
+            // <></> elem inserted here
+            // console.log("This is of type string: ", string);
+            this.elems[0].innerHTML = this.elems[0].innerHTML + string;
+        }
+        else{
+            // createElememt here
+            // console.log("Not of type string: ", string);
+            this.elems[0].appendChild(string.parentNode);
+            // console.log(string.parentNode);
+        }
+        return this;
+    }
+
+    MakeBelieveJS.prototype.prepend = function(string){
+        console.log("We are in ble: ", string);
+        if(typeof string == "string"){
+            // <></> elem inserted here
+            this.elems[0].innerHTML = string + this.elems[0].innerHTML;
+        }
+        else{
+            // createElememt here
+            this.elems[0].insertBefore(string.parentNode, this.elems[0].childNodes[0]);
+        }
+        return this;
+    }
+
     function input(html) {
         var inputs = document.querySelectorAll(html);
         console.log(inputs);
@@ -65,8 +104,25 @@ globalObj.__ =  input;
 // console.log(window);
 
 var my_inputs = __("#my-form input");
-console.log(my_inputs);
+// console.log(my_inputs);
 
-__("input").parent("form").parent();
-// var hello = __(".container").parent();
-var grandParent = __("#password").grandParent(".formdiv");
+// __("input").parent("form").parent();
+// // var hello = __(".container").parent();
+// var grandParent = __("#password").grandParent(".formdiv");
+__("#paragraph").insertText("Suck my dick!");
+
+__(".the-appender").append("<p>I am an appended paragraph!</p>");
+__(".the-appender").append(
+    document.createElement("p")
+        .appendChild(
+            document.createTextNode("I am a special paragraph!")
+        )
+);
+
+__(".the-prepender").prepend("<p>I am an pretended paragraph!</p>");
+__(".the-prepender").prepend(
+    document.createElement("p")
+        .appendChild(
+            document.createTextNode("I am a special pretended paragraph!")
+        )
+);
